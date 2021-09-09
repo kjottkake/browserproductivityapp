e4e2c16abbeaaf347c5fa548256e28bf5925731a
kjottkake <38797885+kjottkake@users.noreply.github.com>
2021-09-09 21:07:19 +0200

    start stop functional

--- timer.js
+++ timer.js
@@ -1,20 +1,28 @@
+let scores = [];
+let startTime;
+let endTime;
+
 //start timer
 function startTimer(){
-    const time = new Date();
-    let hour = time.getHours();
-    let minute = time.getMinutes();
-    let second = time.getSeconds();
-    let startTime = ((hour * 60) + (minute * 60) + second);
-    console.log(startTime);
+    startTime = getTime();
+    alert("timer started!");
 }
 
 //stop timer
     //add to array
 function stopTimer(){
+    endTime = getTime() - startTime;
+    scores.push(endTime);
+    console.table(scores);
+}
+
+
+//get time function
+function getTime(){
     const time = new Date();
     let hour = time.getHours();
     let minute = time.getMinutes();
     let second = time.getSeconds();
-    let startTime = ((hour * 60) + (minute * 60) + second);
-    console.log(startTime);
+    let currentTimeInSeconds = ((hour * 60) + (minute * 60) + second);
+    return currentTimeInSeconds;
 }
