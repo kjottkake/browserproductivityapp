263aef1ada53fa53d5d0723deeffd062e9e158ee
kjottkake <38797885+kjottkake@users.noreply.github.com>
2021-09-10 16:22:27 +0200

    can sort scores

--- timer.js
+++ timer.js
@@ -1,13 +1,16 @@
 // TODO:
 //     [] get timer to display time
 //     [] insert times into Array.
-//     [] display times in correct order
+//     [x] display times in correct order
 
 const leaderboard = document.getElementById('topScore');
 
 let scores = [];
 let startTime;
 let endTime;
+
+
+
 
 //start timer
 function startTimer(){
@@ -21,9 +24,9 @@ function stopTimer(){
     endTime = getTime() - startTime;
     scores.push(endTime);   //puts endtime into scores
     console.table(scores); //test output
-    const topScoreLi = document.createElement('li');
-    topScoreLi.innerHTML = endTime;
-    leaderboard.appendChild(topScoreLi);
+    // const topScoreLi = document.createElement('li');
+    // topScoreLi.innerHTML = endTime;
+    // leaderboard.appendChild(topScoreLi);
 }
 
 
@@ -35,4 +38,31 @@ function getTime(){
     let second = time.getSeconds();
     let currentTimeInSeconds = ((hour * 60) + (minute * 60) + second);
     return currentTimeInSeconds;
+}
+
+function sortTopScores(){
+    scores.sort(function(a, b){return b - a});
+    console.table(scores);
+
+    // //add to array
+    // const topScoreLi = document.createElement('li');
+    // // topScoreLi.innerHTML = endTime;
+    // for (i=0; i< scores.length; i ++){
+    //     // leaderboard.appendChild(scores[i]); 
+    //     topScoreLi.innerHTML = scores[i];
+    // }
+    // leaderboard.appendChild(topScoreLi);
+    showLeader();
+    
+}
+
+function showLeader(){
+    //add to array
+    const topScoreLi = document.createElement('li');
+    // topScoreLi.innerHTML = endTime;
+    for (i=0; i< scores.length; i ++){
+        // leaderboard.appendChild(scores[i]); 
+        topScoreLi.innerHTML = scores[i];
+    }
+    leaderboard.appendChild(topScoreLi);
 }
