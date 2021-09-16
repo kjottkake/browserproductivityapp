cae772e715a88e557b27c21fe79b988f9c68bdbc
kjottkake <38797885+kjottkake@users.noreply.github.com>
2021-09-16 13:41:22 +0200

    timer displays time

--- timer.js
+++ timer.js
@@ -1,22 +1,38 @@
 // TODO:
-//     [] get timer to display time
+//     [X] get timer to display time
+//     [] Get timer to stop display time when stop button is pressed
 //     [x] insert times into Array.
 //     [x] display times in correct order
 //     [] Add miliseconds
 
 const leaderboard = document.getElementById('topScore');
+const watch = document.querySelector("#stopwatch");
 
 let scores = [];
 let startTime;
 let endTime;
 
-
+//defines variables copied from stopwatch.js
+let millisecoundSW = 0;
+let timerSW;
+let secondSW = 0;
 
 
 //start timer
 function startTimer(){
     startTime = getTime();
     console.log("started timer!");
+
+    timerSW = setInterval(() => {
+        millisecoundSW += 10;
+        let dateTimer = new Date(millisecoundSW);
+
+        watch.innerHTML =
+            ('0' + dateTimer.getUTCHours()).slice(-2) + ':' +
+            ('0' + dateTimer.getUTCMinutes()).slice(-2) + ':' +
+            ('0' + dateTimer.getUTCSeconds()).slice(-2) + ':' +
+            ('0' + dateTimer.getUTCMilliseconds()).slice(-3, -1);
+    }, 10);
 }
 
 //stop timer
@@ -25,7 +41,7 @@ function stopTimer(){
     endTime = getTime() - startTime; //gets endTime via subtracting from getTime function
     scores.push(endTime);   //puts endtime into scores
     console.table(scores); //test output
-    sortTopScores();
+    // sortTopScores();
 }
 
 
@@ -35,6 +51,7 @@ function getTime(){
     let hour = time.getHours();
     let minute = time.getMinutes();
     let second = time.getSeconds();
+    // let miliseconds = time.getMilliseconds();
     let currentTimeInSeconds = ((hour * 60) + (minute * 60) + second);
     return currentTimeInSeconds;
 }
@@ -43,28 +60,17 @@ function resetScores(){
     leaderboard.remove(); //once runs doesn't allow adding to li via DOM <-needs to be fixed.
 }
 
+//function sorts topscores.
 function sortTopScores(){
     scores.sort(function(a, b){return b - a});
     console.table(scores);
 
+    //doesn't replace old scores.
     for (i = 0; i < scores.length; i++){
     const topScoreLi = document.createElement('li');
     topScoreLi.innerHTML = scores[i];
     leaderboard.appendChild(topScoreLi);
     }
     
-    // updateLeader();
 }
 
-// function updateLeader(){
-//     //add to array
-//     const topScoreLi = document.createElement('li');
-//     // topScoreLi.innerHTML = endTime;
-
-//     // for (i=0; i< scores.length; i ++){
-//     //     topScoreLi.innerHTML = scores[i];
-//     //     leaderboard.appendChild(topScoreLi);
-//     // }
-//     topScoreLi.innerHTML = scores;
-//     leaderboard.appendChild(topScoreLi);
-// }
